517685067a6c935855f3c7d1d2df9607cae2bc2a
William Trindade <williamtrindade777@gmail.com>
2019-12-30 00:28:56 -0300

    final

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
-const stonesCounter = Math.floor(Math.random() * 21);
-let checkeds = 0;
+let stonesCounter = Math.floor(Math.random() * 21);
+
+let marked = 0;
 
 let playerOne = {
 	'name': 'Player One',
@@ -9,6 +10,29 @@ let playerOne = {
 let playerTwo = {
 	'name': 'Player Two',
 	'backgounrd': 'white',
+}
+
+let turn = playerOne;
+
+function resetFront() {
+	document.querySelector('#player-one-card').style.backgroundColor = 'white';
+	document.querySelector('#player-two-card').style.backgroundColor = 'white';
+	document.querySelector('#player-one-name').style.color = 'black';
+	document.querySelector('#player-two-name').style.color = 'black';
+}
+
+function changeTurnFront() {
+	if (turn === playerOne) {
+		document.querySelector('#player-one-card').style.backgroundColor = 'green';
+		document.querySelector('#player-two-card').style.backgroundColor = 'white';
+		document.querySelector('#player-one-name').style.color = 'white';
+		document.querySelector('#player-two-name').style.color = 'black';
+	} else {
+		document.querySelector('#player-two-card').style.backgroundColor = 'green';
+		document.querySelector('#player-one-card').style.backgroundColor = 'white';
+		document.querySelector('#player-two-name').style.color = 'white';
+		document.querySelector('#player-one-name').style.color = 'black';
+	}
 }
 
 function generateStones(stonesCounter) {
@@ -25,28 +49,28 @@ function generateStones(stonesCounter) {
 	}
 }
 
-function changeStoneStatus(checkeds) {
+function changeStoneStatus() {
 	const stones = document.querySelectorAll('.stone');
 	for (let stone of stones) {
 		stone.addEventListener('click', function(e) {
 			if (e.target.tagName === "IMG") {
 				if (e.target.getAttribute('src') === 'img/stch.png') {
 					e.target.setAttribute('src', 'img/st.png');
-					checkeds--;
+					marked--;
 				} else {
-					if (checkeds != 3) {
+					if (marked != 3) {
 						e.target.setAttribute('src', 'img/stch.png');
-						checkeds++;
+						marked++;
 					}
 				}
 			} else {
 				if (e.target.firstChild.getAttribute('src') === 'img/stch.png') {
 					e.target.firstChild.setAttribute('src', 'img/st.png');
-					checkeds--;
+					marked--;
 				} else {
-					if (checkeds != 3) {
+					if (marked != 3) {
 						e.target.firstChild.setAttribute('src', 'img/stch.png');
-						checkeds++;
+						marked++;
 					}
 				}
 			}
@@ -54,5 +78,52 @@ function changeStoneStatus(checkeds) {
 	}
 }
 
+function resetGame() {
+	marked = 0;
+	turn = playerOne;
+	resetFront();
+	changeTurnFront();
+	generateStones(Math.floor(Math.random() * 21));
+	changeStoneStatus();
+	play();
+}
+
+function play() {
+	const playButton = document.querySelector('#play-button');
+	playButton.addEventListener('click', () => {
+		if (marked === 0 || marked > 3) {
+			alert('Selecione pedras para jogar!');
+		} else {
+			let player = turn;
+			const stones = document.querySelectorAll('.stone');
+			for (let stone of stones) {
+				if (stone.tagName === 'IMG') {
+					if (stone.getAttribute('src') === 'img/stch') {
+						stone.remove();
+					}
+				} else {
+					if (stone.firstChild.getAttribute('src') === 'img/stch.png') {
+						stone.remove();
+					}
+				}
+			}
+			marked = 0;
+			const actualStones = document.querySelectorAll('.stone');
+			if (actualStones.length === 0 ) {
+				alert('O jogador ' + turn.name + ' ganhou o Jogo, pois retirou a última pedra!');
+				resetGame();
+			} else {
+				turn = (turn === playerOne) ? playerTwo : playerOne;
+				changeTurnFront();
+			}
+		}
+	});
+}
+
+changeTurnFront();
+
 generateStones(stonesCounter);
-changeStoneStatus(checkeds);
+
+changeStoneStatus();
+
+play();
